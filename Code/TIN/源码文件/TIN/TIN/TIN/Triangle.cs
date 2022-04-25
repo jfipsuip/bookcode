68c408da140be2e03c5a076141642eeca948b46b
jfipsuip <jfipsuip@126.com>
2022-04-25 11:09:34 +0800

    添加 Triangle.cs

---
+++ Code/TIN/源码文件/TIN/TIN/TIN/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIN.TIN
+{
+    /// <summary>
+    /// 三角形
+    /// </summary>
+    public class Triangle
+    {
+        /// <summary>
+        /// 三角形的第一个点A
+        /// </summary>
+        public Point PointA { get; set; }
+        /// <summary>
+        /// 三角形的第二个点B
+        /// </summary>
+        public Point PointB { get; set; }
+        /// <summary>
+        /// 三角形的第一个点C
+        /// </summary>
+        public Point PointC { get; set; }
+    }
+}

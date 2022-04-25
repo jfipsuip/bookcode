3998b83bbd150dea241747d6655fa92bdd291dd7
jfipsuip <jfipsuip@126.com>
2022-04-25 11:08:24 +0800

    添加 Test.cs

---
+++ Code/TIN/源码文件/TIN/TIN/TIN/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIN.TIN
+{
+    public class Test
+    {
+        public static void Usage()
+        {
+            string str = "P01,3778.594,2885.732,9.468";
+            Point point = new Point(str);
+        }
+    }
+}

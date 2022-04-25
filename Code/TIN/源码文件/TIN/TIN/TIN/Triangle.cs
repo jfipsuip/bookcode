61344a32e90a068d12491dcd8432dd89930152ae
jfipsuip <jfipsuip@126.com>
2022-04-25 11:34:26 +0800

    计算三角的外接圆的圆心和半径

--- Code/TIN/源码文件/TIN/TIN/TIN/Triangle.cs
+++ Code/TIN/源码文件/TIN/TIN/TIN/Triangle.cs
@@ -23,5 +23,60 @@ namespace TIN.TIN
         /// 三角形的第一个点C
         /// </summary>
         public Point PointC { get; set; }
+
+        /// <summary>
+        /// 三角形外接圆圆心 Xo 坐标
+        /// </summary>
+        public double X
+        {
+            get
+            {
+                return GetX(PointA.X, PointB.X, PointC.X, PointA.Y, PointB.Y, PointC.Y);
+            }
+        }
+        /// <summary>
+        /// 三角形外接圆圆心 Yo 坐标
+        /// </summary>
+        public double Y
+        {
+            get
+            {
+                return GetY(PointA.X, PointB.X, PointC.X, PointA.Y, PointB.Y, PointC.Y);
+            }
+        }
+        /// <summary>
+        /// 三角形外接圆半径 r
+        /// </summary>
+        public double R
+        {
+            get
+            {
+                return GetR(X, Y, PointA.X, PointB.Y);
+            }
+        }
+
+
+
+        public Triangle(Point pointA, Point pointB, Point pointC)
+        {
+            PointA = pointA;
+            PointB = pointB;
+            PointC = pointC;
+        }
+        private double GetX(double x1, double x2, double x3, double y1, double y2, double y3)
+        {
+            double x = ((y2 - y1) * (y3 * y3 - y1 * y1 + x3 * x3 - x1 * x1) - (y3 - y1) * (y2 * y2 - y1 * y1 + x2 * x2 - x1 * x1)) / (2 * (x3 - x1) * (y2 - y1) - 2 * (x2 - x1) * (y3 - y1));
+            return x;
+        }
+        private double GetY(double x1, double x2, double x3, double y1, double y2, double y3)
+        {
+            double y = ((x2 - x1) * (x3 * x3 - x1 * x1 + y3 * y3 - y1 * y1) - (x3 - x1) * (x2 * x2 - x1 * x1 + y2 * y2 - y1 * y1)) / (2 * (y3 - y1) * (x2 - x1) - 2 * (y2 - y1) * (x3 - x1));
+            return y;
+        }
+        private double GetR(double x, double y, double x1, double y1)
+        {
+            double r = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+            return r;
+        }
     }
 }

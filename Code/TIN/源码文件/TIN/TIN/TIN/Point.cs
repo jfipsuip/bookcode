5cc49688069e2266e0d84138b7bab4fa47a9cf32
jfipsuip <jfipsuip@126.com>
2022-04-25 11:07:22 +0800

    添加 Point.cs

---
+++ Code/TIN/源码文件/TIN/TIN/TIN/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIN.TIN
+{
+    /// <summary>
+    /// 点
+    /// </summary>
+    public class Point
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// X坐标
+        /// </summary>
+        public double X { get; set; }
+        /// <summary>
+        /// Y坐标
+        /// </summary>
+        public double Y { get; set; }
+        /// <summary>
+        /// H坐标
+        /// </summary>
+        public double H { get; set; }
+
+        public Point(string line)
+        {
+            string[] strs = line.Split(',');
+            Name = strs[0];
+            X = Convert.ToDouble(strs[1]);
+            Y = Convert.ToDouble(strs[2]);
+            H = Convert.ToDouble(strs[3]);
+        }
+    }
+}

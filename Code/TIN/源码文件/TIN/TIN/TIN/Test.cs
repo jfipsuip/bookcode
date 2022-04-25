a8173323920e10bcafdb50ece766b52f46786e43
jfipsuip <jfipsuip@126.com>
2022-04-25 11:34:52 +0800

    测试 计算三角的外接圆的圆心和半径

--- Code/TIN/源码文件/TIN/TIN/TIN/Test.cs
+++ Code/TIN/源码文件/TIN/TIN/TIN/Test.cs
@@ -8,10 +8,16 @@ namespace TIN.TIN
 {
     public class Test
     {
-        public static void Usage()
+        public static void Usage1()
         {
             string str = "P01,3778.594,2885.732,9.468";
             Point point = new Point(str);
+        }
+        public static void Usage()
+        {
+
+            Triangle triangle = new Triangle(new Point("P1,0,0,0"), new Point("P1,4,0,0"), new Point("P1,0,2,0"));
+
         }
     }
 }

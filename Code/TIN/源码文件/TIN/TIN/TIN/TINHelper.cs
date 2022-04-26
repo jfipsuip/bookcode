82cb5f365b5f8478308b8782148d535fe97a202d
jfipsuip <jfipsuip@126.com>
2022-04-26 11:09:04 +0800

    获取边列表S

--- Code/TIN/源码文件/TIN/TIN/TIN/TINHelper.cs
+++ Code/TIN/源码文件/TIN/TIN/TIN/TINHelper.cs
@@ -26,11 +26,72 @@ namespace TIN.TIN
                     if (triangle.IsInCircle(point))
                     {
                         t2.Add(triangle);
-                        t1.Remove(triangle);
                     }
                 }
+                // 从T1移除
+                foreach (var triangle in t2)
+                {
+                    t1.Remove(triangle);
+                }
+                // 1.3.4 获取边列表S;
+                List<Side> sides = GetSide(t2);
             }
         }
+
+        private static List<Side> GetSide(List<Triangle> triangles)
+        {
+            List<Side> sides, result;
+
+            sides = new List<Side>();
+            // 获取三角形列表的所有边
+            foreach (var t in triangles)
+            {
+                sides.Add(new Side(t.PointA, t.PointB));
+                sides.Add(new Side(t.PointA, t.PointC));
+                sides.Add(new Side(t.PointB, t.PointC));
+            }
+
+            // 获取删除公共边后的边列表 
+            result = GetSide(sides);
+            // 清空T2
+            triangles = new List<Triangle>();
+
+            return sides;
+        }
+        /// <summary>
+        /// 获取删除公共边后的边列表
+        /// </summary>
+        /// <param name="sides"></param>
+        /// <returns></returns>
+        private static List<Side> GetSide(List<Side> sides)
+        {
+            List<Side> result = new List<Side>();
+
+            foreach (var side in sides)
+            {
+                if (IsCommon(sides, side) == false)
+                {
+                    result.Add(side);
+                }
+            }
+
+            return result;
+
+        }
+
+        private static bool IsCommon(List<Side> sides, Side sideA)
+        {
+            foreach (var sideB in sides)
+            {
+                if (sideA != sideB && sideA.IsCommonSide(sideB))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         /// <summary>
         /// 获得初始三角形列表
         /// </summary>
@@ -45,7 +106,7 @@ namespace TIN.TIN
             xMax = points.Select(t => t.X).Max();
             xMin = points.Select(t => t.X).Min();
             yMax = points.Select(t => t.Y).Max();
-            yMin = points.Select(t => t.Y).Max();
+            yMin = points.Select(t => t.Y).Min();
 
             p1 = new Point("P1", xMin - 1, yMin - 1);
             p2 = new Point("P2", xMin - 1, yMax + 1);

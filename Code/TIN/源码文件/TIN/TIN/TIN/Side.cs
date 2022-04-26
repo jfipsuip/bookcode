bdb3a906028a59866d374017e05a9880eea80651
jfipsuip <jfipsuip@126.com>
2022-04-26 10:09:27 +0800

    添加 边 类

---
+++ Code/TIN/源码文件/TIN/TIN/TIN/Side.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIN.TIN
+{
+    /// <summary>
+    /// 边
+    /// </summary>
+    public class Side
+    {
+        public Point PointA { get; set; }
+        public Point PointB { get; set; }
+
+        public Side(Point pointA, Point pointB)
+        {
+            PointA = pointA;
+            PointB = pointB;
+        }
+
+        /// <summary>
+        /// 判断边是否为公共边
+        /// </summary>
+        /// <param name="side"></param>
+        /// <returns></returns>
+        public bool IsCommonSide(Side side)
+        {
+            bool isCommon = false;
+
+            if (PointA == side.PointA && PointB == side.PointB)
+            {
+                isCommon = true;
+            }
+            else if (PointA == side.PointB && PointB == side.PointA)
+            {
+                isCommon = true;
+            }
+
+            return isCommon;
+        }
+
+        private static void adfdf()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

488937960e9a4dbce2b62029d070577aaa74f38d
jfipsuip <jfipsuip@126.com>
2022-04-27 16:20:09 +0800

    添加 Common.cs

---
+++ Code/TIN/源码文件/TIN/TIN/TIN/Common.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIN.TIN
+{
+    public class Common
+    {
+        public static void NewFile()
+        {
+
+        }
+
+
+        public static void BindData(DataGridView data, string[] lines)
+        {
+            string[] strs;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                strs = lines[i].Split(',');
+                data[0, i].Value = strs[0];
+                data[1, i].Value = strs[1];
+                data[2, i].Value = strs[2];
+                data[3, i].Value = strs[3];
+            }
+        }
+        public static DataGridView NewGrid(DataGridView data, int n = 11)
+        {
+            data.ColumnCount = 4;
+            data.RowCount = n;
+
+            for (int i = 0; i < data.ColumnCount; i++)
+            {
+                data.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                data.Columns[i].Width = (data.Width - 20) / data.ColumnCount;
+            }
+            data.RowHeadersVisible = false;
+            data.Columns[0].HeaderText = "点名";
+            data.Columns[1].HeaderText = "X分量";
+            data.Columns[2].HeaderText = "Y分量";
+            data.Columns[3].HeaderText = "H分量";
+
+            data.Show();
+            return data;
+        }
+
+        public static void Calculate(DataGridView dataGrid)
+        {
+            List<Point> points;
+
+            points = GetPoints(dataGrid);
+
+            var tin = TINHelper.GetTIN(points, 9);
+
+        }
+        private static List<Point> GetPoints(DataGridView dataGrid)
+        {
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < dataGrid.Rows.Count; i++)
+            {
+                Point point = GetPoint(dataGrid.Rows[i]);
+                points.Add(point);
+            }
+
+            return points;
+        }
+
+        private static Point GetPoint(DataGridViewRow dataGrid)
+        {
+            string name;
+            double x, y, h;
+            Point point;
+
+            name = Convert.ToString(dataGrid.Cells[0].Value);
+            x = Convert.ToDouble(dataGrid.Cells[1].Value);
+            y = Convert.ToDouble(dataGrid.Cells[2].Value);
+            h = Convert.ToDouble(dataGrid.Cells[3].Value);
+
+            point = new Point(name, x, y, h);
+
+            return point;
+        }
+    }
+}

fcb10d3601e39c5ae1f184de6c2eb59603a8c062
jfipsuip <jfipsuip@126.com>
2022-05-02 09:44:11 +0800

    添加 Grid.cs

---
+++ Code/Grid/源码文件/Grid/Grid/Grids/Grid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid.Grids
+{
+    public class Grid
+    {
+        public List<Point> Points { get; }
+        /// <summary>
+        /// 基准高程
+        /// </summary>
+        public double H { get; set; }
+        public Grid(List<Point> points, double h)
+        {
+            Points = points;
+            H = h;
+        }
+    }
+}

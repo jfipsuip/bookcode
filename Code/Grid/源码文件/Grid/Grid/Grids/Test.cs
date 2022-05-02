12d29e8f922cc578f9a1fabcb68bc9c5fcf70ae8
jfipsuip <jfipsuip@126.com>
2022-05-02 10:08:02 +0800

    添加 Test.cs

---
+++ Code/Grid/源码文件/Grid/Grid/Grids/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid.Grids
+{
+    public class Test
+    {
+        public static void Usage()
+        {
+            string path = @"D:\github\jfipsuip\bookcode\Code\Grid\测试数据\GRID数据.txt";
+            string[] lines = File.ReadAllLines(path);
+            string line = lines[0];
+            double h = Convert.ToDouble(line.Split(',')[1]);
+            string[] linePoints = lines.Skip(3).ToArray();
+
+            List<Point> points = linePoints.Select(t => new Point(t)).ToList();
+            Grid grid = new Grid(points, h);
+
+        }
+    }
+}

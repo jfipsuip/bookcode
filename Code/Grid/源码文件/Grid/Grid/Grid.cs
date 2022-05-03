edf902d2ac62f288a4d152d062e1d771e922863d
jfipsuip <jfipsuip@126.com>
2022-05-03 09:59:09 +0800

    复制页面文件

---
+++ Code/Grid/源码文件/Grid/Grid/Grid.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TIN
+{
+    public partial class Grid : Form
+    {
+
+        public Grid()
+        {
+            InitializeComponent();
+            point_h.Checked = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void GetPic_Point(double zoom, Point go)
+        {
+        }
+
+
+
+
+
+        private void Grid_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 新建数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 打开文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Tin计算
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+        }
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void 数据表格ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void 示意图ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void 计算报告ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Document_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 计算三角网及体积ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void 新建NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void 保存SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 查看帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void toolStripButton8_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void point_h_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void point_h_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}

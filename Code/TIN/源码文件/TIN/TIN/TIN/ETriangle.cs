8d4876cdcdc3f0d55cb2cf9da324999c2afa6ebf
jfipsuip <jfipsuip@126.com>
2022-05-14 09:02:37 +0800

    ETriangle.cs

---
+++ Code/TIN/源码文件/TIN/TIN/TIN/ETriangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIN.TIN
+{
+    public enum ETriangle
+    {
+        全挖方,
+        全填方,
+        两个顶点低,
+        一个顶点低
+    }
+}

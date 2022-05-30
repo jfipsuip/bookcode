07ccc946836d80de219a9928cee361fb7526f1a3
jfipsuip <jfipsuip@126.com>
2022-05-30 10:38:45 +0800

    添加菜单

--- Code/Demo/源码文件/Surveying/Surveying/Form1.Designer.cs
+++ Code/Demo/源码文件/Surveying/Surveying/Form1.Designer.cs
@@ -29,20 +29,139 @@ namespace Surveying
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.算法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.显示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存程序正确性ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存计算结果ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存成果图形ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.放大ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.缩小ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.算法ToolStripMenuItem,
+            this.显示ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1270, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.保存ToolStripMenuItem,
+            this.保存程序正确性ToolStripMenuItem,
+            this.保存计算结果ToolStripMenuItem,
+            this.保存成果图形ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 算法ToolStripMenuItem
+            // 
+            this.算法ToolStripMenuItem.Name = "算法ToolStripMenuItem";
+            this.算法ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.算法ToolStripMenuItem.Text = "算法";
+            // 
+            // 显示ToolStripMenuItem
+            // 
+            this.显示ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.放大ToolStripMenuItem,
+            this.缩小ToolStripMenuItem});
+            this.显示ToolStripMenuItem.Name = "显示ToolStripMenuItem";
+            this.显示ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.显示ToolStripMenuItem.Text = "显示";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            // 
+            // 保存程序正确性ToolStripMenuItem
+            // 
+            this.保存程序正确性ToolStripMenuItem.Name = "保存程序正确性ToolStripMenuItem";
+            this.保存程序正确性ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存程序正确性ToolStripMenuItem.Text = "保存程序正确性";
+            // 
+            // 保存计算结果ToolStripMenuItem
+            // 
+            this.保存计算结果ToolStripMenuItem.Name = "保存计算结果ToolStripMenuItem";
+            this.保存计算结果ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存计算结果ToolStripMenuItem.Text = "保存计算结果";
+            // 
+            // 保存成果图形ToolStripMenuItem
+            // 
+            this.保存成果图形ToolStripMenuItem.Name = "保存成果图形ToolStripMenuItem";
+            this.保存成果图形ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存成果图形ToolStripMenuItem.Text = "保存成果图形";
+            // 
+            // 放大ToolStripMenuItem
+            // 
+            this.放大ToolStripMenuItem.Name = "放大ToolStripMenuItem";
+            this.放大ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.放大ToolStripMenuItem.Text = "放大";
+            // 
+            // 缩小ToolStripMenuItem
+            // 
+            this.缩小ToolStripMenuItem.Name = "缩小ToolStripMenuItem";
+            this.缩小ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.缩小ToolStripMenuItem.Text = "缩小";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1270, 675);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存程序正确性ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存计算结果ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存成果图形ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 算法ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 放大ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 缩小ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
     }
 }
 
